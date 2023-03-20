ce5370203e3d49f8d60ae2fdb8e44664d1b39307
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-03-20 21:40:44 +0300

    Update and rename q.cpp to counting at the "add" stage.cpp

---
+++ занятие 8/A. Высота дерева/counting at the "add" stage.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int NodeHeight = 1;
+int TreeHeight = 0;
+
+struct Node {
+    int value;
+    Node* left = nullptr;
+    Node* right = nullptr;
+
+    Node() = default;
+    Node(int value) : value(value) {}
+};
+
+
+Node* add(Node* node, int value) {
+
+
+    if (node == nullptr) { 
+        TreeHeight = max(TreeHeight, NodeHeight); 
+        return new Node(value); 
+    }
+
+    ++NodeHeight;
+    if (value < node->value) {
+        node->left = add(node->left, value);
+    }
+    if (value > node->value) {
+        node->right = add(node->right, value);
+    }
+
+
+    return node;
+}
+
+
+int main() {
+
+    Node* tr = nullptr;
+
+    while (true) {
+        int x;
+        cin >> x;
+
+        if (x == 0) break;
+
+        tr = add(tr, x);
+        NodeHeight = 1;
+    }
+
+    cout << TreeHeight;
+    return 0;
+
+}

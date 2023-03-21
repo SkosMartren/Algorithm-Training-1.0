4ecbdd579ba817a9fd936658da654a472d8f06f5
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-03-21 17:50:46 +0300

    Create modified Inorder -tree traversal.cpp

---
+++ занятие 8/F. Вывод развилок/modified Inorder -tree traversal.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node {
+	int value;
+	Node* left = nullptr;
+	Node* right = nullptr;
+
+	Node() = default;
+	Node(int value) : value(value) {}
+};
+
+
+Node* add(Node* tr, int data) {
+
+	if (tr == nullptr) { return new Node(data); }
+
+	if (data > tr->value) {
+		tr->right = add(tr->right, data);
+	}
+
+	if (data < tr->value) {
+		tr->left = add(tr->left, data);
+	}
+
+	return tr;
+}
+
+
+void Inorder(Node* tr) {
+
+	if (tr == nullptr) { return; }
+  
+  // error!
+  /*  
+  if (tr->left != nullptr && nullptr != tr->right) {
+	cout << tr->value << '\n';
+	}
+  */
+	Inorder(tr->left);
+
+	if (tr->left != nullptr && nullptr != tr->right) {
+		cout << tr->value << '\n';
+	}
+  
+	Inorder(tr->right);
+  // error!
+  /*  
+  if (tr->left != nullptr && nullptr != tr->right) {
+	cout << tr->value << '\n';
+	}
+  */
+
+}
+
+int main() {
+
+	Node* tr = nullptr;
+
+	while (true) {
+
+		int data;
+		cin >> data;
+		if (data == 0) { break; }
+
+		tr = add(tr, data);
+	}
+
+	Inorder(tr);
+}

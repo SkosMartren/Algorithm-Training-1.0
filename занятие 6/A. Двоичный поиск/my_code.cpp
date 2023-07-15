8a8f576fa649dfd3a7c4dfe31c7a52c7563ee8c4
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-16 02:37:14 +0300

    Create my_code.cpp

---
+++ занятие 6/A. Двоичный поиск/my_code.cpp
@@ -0,0 +1,37 @@
+#include<string>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	int N, K; cin >> N >> K;
+
+	vector<int> Nums(N);
+	
+	for (int& num : Nums) {cin >> num;}
+
+	int target;
+	while (cin >> target) {
+		
+		if(target < Nums.front() || Nums.back() < target){cout << "NO\n"; continue;}
+
+		int Left = -1;
+		int Right = N;
+
+		while (1 != Right - Left) {
+
+			const int mid = (Right + Left) / 2;
+
+			if (target <= Nums[mid]) {
+				Right = mid;
+			}
+			else {
+				Left = mid;
+			}
+		}
+
+		cout << ((Nums[Right] == target) ? "YES\n" : "NO\n");
+	}
+}
